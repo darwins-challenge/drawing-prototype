f56909230ee7047fc5d165c6bd5868ff6fb358bc
dvberkel <daan.v.berkel.1980@gmail.com>
2014-10-07 16:17:39 +0200

    draw a number of different programs

--- js/darwin.js
+++ js/darwin.js
@@ -1,4 +1,13 @@
 ;(function($){
+    var range = function(a, b, step) {
+	step = step || 1;
+	var result = [];
+	for (var index = a; index < b; index += step) {
+	    result.push(index);
+	}
+	return result;
+    }
+
     var canvas = document.createElement('canvas');
     canvas.width = 640;
     canvas.height = 640;
@@ -8,13 +17,16 @@
     context.translate(canvas.width/2, canvas.height/2);
     context.scale(1, -1);
 
-    var p = $.language.random.program({
-	sequence: 3/4,
-	action: [ 1/6, 2/6, 5/6 ]
+    range(1,10).map(function(){
+	return $.language.random.program({
+	    sequence: 3/4,
+	    action: [ 1/6, 2/6, 5/6 ]
+	});
+    }).forEach(function(p, index, programs){
+	var angle = 360 * (index/programs.length);
+	p.executeOn(new $.Machine(context, {
+	    size: 10,
+	    color: 'hsl(' + angle + ',100%,50%)'
+	}));
     });
-    p.executeOn(new $.Machine(context, {
-	size: 10,
-	color: 'hsl(0,100%,50%)'
-    }));
-    console.log(p.toString());
 })(window);

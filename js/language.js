34d4ef60fdf5bc30099bd55f3d523daefee63d0d
dvberkel <daan.v.berkel.1980@gmail.com>
2014-10-08 18:13:32 +0200

    create a uniform way to create abstract syntax mode

--- js/language.js
+++ js/language.js
@@ -1,60 +1,29 @@
 ;(function($){
-    var Program = function(action){
+    var Node = function(action, children){
 	this.action = action;
+	this.children = children || [];
     };
-    Program.prototype.executeOn = function(machine){
-	this.action.executeOn(machine);
+    Node.prototype.executeOn = function(machine) {
+	this.action.call(this, machine, this.children);
+    };
+
+    var program = function(machine, children){
+	children.forEach(function(child){ child.executeOn(machine); });
 	machine.finish();
     };
-    Program.prototype.toString = function(){
-	return 'var p;\nwith($.language){\n\tp = program(' + this.action.toString() + ');\n}'
+    var sequence = function(machine, children){
+	children.forEach(function(child){ child.executeOn(machine); });
     };
-
-    var Sequence = function(actions){
-	this.actions = actions;
-    };
-    Sequence.prototype.executeOn = function(machine){
-	this.actions.forEach(function(action){
-	    action.executeOn(machine);
-	});
-    };
-    Sequence.prototype.toString = function(){
-	var result = 'sequence('
-	result += this.actions.map(function(action){ return action.toString(); }).join(',');
-	result += ')';
-	return result;
-    };
-
-    var Left = function(){};
-    Left.prototype.executeOn = function(machine){
-	machine.left();
-    };
-    Left.prototype.toString = function(){
-	return 'left()';
-    };
-
-    var Right = function(){};
-    Right.prototype.executeOn = function(machine){
-	machine.right();
-    };
-    Right.prototype.toString = function(){
-	return 'right()';
-    };
-
-    var Forward = function(){};
-    Forward.prototype.executeOn = function(machine){
-	machine.forward();
-    };
-    Forward.prototype.toString = function(){
-	return 'forward()';
-    };
+    var left = function(machine){ machine.left(); };
+    var right = function(machine){ machine.right(); };
+    var forward = function(machine){ machine.forward(); };
 
     var language = $.language = {};
-    language.program  = function(action){ return new Program(action); };
-    language.sequence = function(){ return new Sequence(Array.prototype.slice.call(arguments)); };
-    language.left     = function(){ return new Left(); };
-    language.right    = function(){ return new Right(); };
-    language.forward  = function(){ return new Forward(); };
+    language.program  = function(action){ return new Node(program, [action]); };
+    language.sequence = function(){ return new Node(sequence, Array.prototype.slice.call(arguments)); };
+    language.left     = function(){ return new Node(left); };
+    language.right    = function(){ return new Node(right); };
+    language.forward  = function(){ return new Node(forward); };
 
     var random = $.language.random = {};
     (function(_){
@@ -69,7 +38,7 @@
 	    while (Math.random() < distribution.sequence) {
 		actions.push(randomAction(distribution));
 	    }
-	    return new Sequence(actions);
+	    return new Node(sequence, actions);
 	};
 	var randomAction = function(distribution){
 	    var p = Math.random();
